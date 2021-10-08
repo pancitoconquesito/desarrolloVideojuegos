97ffb40665745528aaf550cb46c8abe7d8a33dd6
pancitoConQuesito <gonzalo.astorga.@gmail.com>
2021-10-08 17:36:58 -0300

    a

---
+++ src/app/COMPONENTS/gatuh-run/gatuh-run.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gatuh-run',
+  templateUrl: './gatuh-run.component.html',
+  styleUrls: ['./gatuh-run.component.scss']
+})
+export class GatuhRunComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FmcComponent } from './COMPONENTS/fmc/fmc.component';
+import { FooterComponent } from './COMPONENTS/footer/footer.component';
+import { GaleriaImgComponent } from './COMPONENTS/galeria-img/galeria-img.component';
+import { GaleriaGamesComponent } from './COMPONENTS/galeria-games/galeria-games.component';
+import { GatuhRunComponent } from './COMPONENTS/gatuh-run/gatuh-run.component';
+import { HeaderComponent } from './COMPONENTS/header/header.component';
+import { InicioComponent } from './COMPONENTS/inicio/inicio.component';
+import { LmmComponent } from './COMPONENTS/lmm/lmm.component';
+import { MuseoComponent } from './COMPONENTS/museo/museo.component';
+import { NavComponent } from './COMPONENTS/nav/nav.component';
+import { OtrosComponent } from './COMPONENTS/otros/otros.component';
+import { PurucuComponent } from './COMPONENTS/purucu/purucu.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    FmcComponent,
+    FooterComponent,
+    GaleriaImgComponent,
+    GaleriaGamesComponent,
+    GatuhRunComponent,
+    HeaderComponent,
+    InicioComponent,
+    LmmComponent,
+    MuseoComponent,
+    NavComponent,
+    OtrosComponent,
+    PurucuComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    NgbModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
